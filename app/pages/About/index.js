2c10a39b55131d044c7dde997aa3e873e36c520c
Jeet <jeetpatel0070070@gmail.com>
2022-04-25 12:13:40 +0530

    add page transition using ajax

--- app/pages/About/index.js
+++ app/pages/About/index.js
@@ -15,7 +15,11 @@ export default class About extends Page {
       element: ".about",
       elements: {
         wrapper: ".about__wrapper", // scroller
-        about_pics: ".parallax__image"
+        about_pics: ".parallax__image",
+        // sliders
+        menu: ".about__team__menu",
+        items: ".menu__item",
+        images: ".menu__item__pic"
       }
     });
   }
@@ -23,6 +27,7 @@ export default class About extends Page {
   create() {
     super.create();
     this.parallax();
+    this.slider();
   }
 
   parallax() {
@@ -35,13 +40,107 @@ export default class About extends Page {
     });
     // this.imgOne.forEach((ele) => {
     //   this.parallaxEffect = new Ukiyo(ele, {
-    //     speed: 2.5,
-    //     scale: 1.3
+    //     speed: 2,
+    //     scale: 1.4
     //   });
     // });
   }
 
   onResize() {
     super.onResize();
+  }
+
+  slider() {
+    let menuWidth = this.elements.menu.clientWidth;
+    let itemWidth = this.elements.items[0].clientWidth;
+    let wrapWidth = this.elements.items.length * itemWidth;
+
+    let scrollSpeed = 0;
+    let oldScrollY = 0;
+    let scrollY = 0;
+    let y = 0;
+
+    const lerp = (v0, v1, t) => {
+      return v0 * (1 - t) + v1 * t;
+    };
+
+    const dispose = (scroll) => {
+      gsap.set(this.elements.items, {
+        x: (i) => {
+          return i * itemWidth + scroll;
+        },
+        modifiers: {
+          x: (x, target) => {
+            const s = gsap.utils.wrap(
+              -itemWidth,
+              wrapWidth - itemWidth,
+              parseInt(x)
+            );
+            return `${s}px`;
+          }
+        }
+      });
+    };
+    dispose(0);
+
+    const handleMouseWheel = (e) => {
+      scrollY -= e.deltaY * 0.9;
+    };
+
+    let touchStart = 0;
+    let touchX = 0;
+    let isDragging = false;
+    const handleTouchStart = (e) => {
+      touchStart = e.clientX || e.touches[0].clientX;
+      isDragging = true;
+      this.elements.menu.classList.add("is-dragging");
+    };
+    const handleTouchMove = (e) => {
+      if (!isDragging) return;
+      touchX = e.clientX || e.touches[0].clientX;
+      scrollY += (touchX - touchStart) * 2.5;
+      touchStart = touchX;
+    };
+    const handleTouchEnd = () => {
+      isDragging = false;
+      this.elements.menu.classList.remove("is-dragging");
+    };
+
+    this.elements.menu.addEventListener("mousewheel", handleMouseWheel);
+
+    this.elements.menu.addEventListener("touchstart", handleTouchStart);
+    this.elements.menu.addEventListener("touchmove", handleTouchMove);
+    this.elements.menu.addEventListener("touchend", handleTouchEnd);
+
+    this.elements.menu.addEventListener("mousedown", handleTouchStart);
+    this.elements.menu.addEventListener("mousemove", handleTouchMove);
+    this.elements.menu.addEventListener("mouseleave", handleTouchEnd);
+    this.elements.menu.addEventListener("mouseup", handleTouchEnd);
+
+    this.elements.menu.addEventListener("selectstart", () => {
+      return false;
+    });
+
+    window.addEventListener("resize", () => {
+      menuWidth = this.elements.menu.clientWidth;
+      itemWidth = this.elements.items[0].clientWidth;
+      wrapWidth = this.elements.items.length * itemWidth;
+    });
+
+    const render = () => {
+      requestAnimationFrame(render);
+      y = lerp(y, scrollY, 0.1);
+      dispose(y);
+
+      scrollSpeed = y - oldScrollY;
+      oldScrollY = y;
+
+      gsap.to(this.elements.items, {
+        skewX: -scrollSpeed * 0.2,
+        rotate: scrollSpeed * 0.01
+        // scale: 1 - Math.min(100, Math.abs(scrollSpeed)) * 0.003
+      });
+    };
+    render();
   }
 }
--- app/shaders/frag.glsl
+++ app/shaders/frag.glsl
@@ -1,31 +1,10 @@
-precision highp float;
+ uniform sampler2D baseTexture;
+  uniform sampler2D bloomTexture;
 
-  uniform sampler2D t1;
-  varying vec3 vNormal;
-  varying vec3 eyeVector;
-  float PI = 3.141592653589793238;
+  varying vec2 vUv;
 
-  vec2 hash22(vec2 p) {
-    p = fract(p * vec2(5.3983, 5.4427));
-    p += dot(p.xy, p.xy + vec2(21.5351, 14.3137));
-    return fract(vec2(p.x * p.y * 95.4337, p.x * p.y * 97.597));
-  }
+  void main() {
 
-  void main()	{
-    vec3 X = dFdx(vNormal);
-    vec3 Y = dFdy(vNormal);
-    vec3 normal = normalize(cross(X, Y));
-    float diffuse = dot(normal, vec3(1.0));
-    vec2 rand = hash22(vec2(floor(diffuse * 10.0)));
-    vec2 uvv = vec2(
-      sign(rand.x - 0.5) * 1.0 + (rand.x - 0.5) * 0.6,
-      sign(rand.y - 0.5) * 1.0 + (rand.y - 0.5) * 0.6
-    );
-    float fresnel = pow(1.0 + dot(eyeVector, normal), 2.0);
-    vec2 uv = uvv * gl_FragCoord.xy / vec2(1000.0);
-    vec3 refracted = refract(eyeVector, normal, 1.0 / 3.0);
-    uv += 0.2 * refracted.xy;
-    vec4 t = texture2D(t1, uv);
+    gl_FragColor = ( texture2D( baseTexture, vUv ) + vec4( 1.0 ) * texture2D( bloomTexture, vUv ) );
 
-    gl_FragColor = t * (1.0 - fresnel);
   }
